526d4b8c3f906cb3259cf9456ad4a25c10f011da
cody dullard <cody.dullard2@mail.dcu.ie>
2020-12-15 15:49:21 +0000

    Split test data into its own file.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,33 +1,8 @@
 import { Component, React } from 'react';
+import data from "./testData"
 import DataTable from 'react-data-table-component';
 
 const loggedStudent = "test1"
-
-const data = [
-    {
-        company: "Google",
-        job_title: "SWE Intern",
-        courses: "EC / CASE",
-        final_date: "19-01-2021",
-        description: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget commodo libero. Mauris congue consectetur vulputate. Maecenas magna dui, ultrices id fringilla ut, pulvinar eget ex. Aenean eu urna neque. Maecenas quis diam eu elit maximus consectetur eget et sem. Vivamus in purus dignissim, pharetra magna ut, vehicula urna. Pellentesque non bibendum nisl. Donec volutpat ornare eros id commodo. Cras ut venenatis massa, imperdiet pellentesque magna. ",
-        applied_students: ["test1"]
-    },
-    {
-        company: "Amazon",
-        job_title: "SDE Intern",
-        courses: "EC / CASE",
-        final_date: "15-03-2021",
-        description: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget commodo libero. Mauris congue consectetur vulputate. Maecenas magna dui, ultrices id fringilla ut, pulvinar eget ex. Aenean eu urna neque. Maecenas quis diam eu elit maximus consectetur eget et sem. Vivamus in purus dignissim, pharetra magna ut, vehicula urna. Pellentesque non bibendum nisl. Donec volutpat ornare eros id commodo. Cras ut venenatis massa, imperdiet pellentesque magna. ",
-        applied_students: ["test2", "test3"]
-    },
-    {
-        company: "Demonware",
-        job_title: "SRE Intern",
-        courses: "CASE",
-        final_date: "10-12-2020",
-        description: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget commodo libero. Mauris congue consectetur vulputate. Maecenas magna dui, ultrices id fringilla ut, pulvinar eget ex. Aenean eu urna neque. Maecenas quis diam eu elit maximus consectetur eget et sem. Vivamus in purus dignissim, pharetra magna ut, vehicula urna. Pellentesque non bibendum nisl. Donec volutpat ornare eros id commodo. Cras ut venenatis massa, imperdiet pellentesque magna. ",
-        applied_students: ["test3"]
-    }]
 
 const columns = [
     {
