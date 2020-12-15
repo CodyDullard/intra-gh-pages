26eaf5c3bf2e6d2e4a256a9497643c8a33dcdb07
ocionnm3 <maitiu.ocionnaith3@mail.dcu.ie>
2020-12-15 17:58:53 +0000

    add login page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,39 @@
 function Home() {
     return (
-      <div>
-        <h2>Home</h2>
-      </div>
-    );
+	 <div>
+        <h2>Student Login</h2>
+        <form>
+		<label>Username</label>
+		<input placeholder="username" type="text" required/>
+		<label>Password</label>
+		<input placeholder="password" type="password" required/>
+		<button type="button">login</button>
+		</form>
+		
+		<h2>Company Login</h2>
+		<form>
+		<label>Username</label>
+		<input placeholder="username" type="text" required/>
+		<label>Password</label>
+		<input placeholder="password" type="password" required/>
+		<button type="button">login</button>
+		</form>
+		
+		
+		<h2>INTRA Office Login</h2>
+		<form>
+		<label>Username</label>
+		<input placeholder="username" type="text" required/>
+		<label>Password</label>
+		<input placeholder="password" type="password" required/>
+		<button type="button">login</button>
+		</form>
+		
+		
+	 </div> 
+
+    
+		);
   }
   
 
