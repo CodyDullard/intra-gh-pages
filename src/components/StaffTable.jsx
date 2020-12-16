d6112d4fb3e7947424d1bea11de61ce6dd6025c9
Jude Laguipo <jude.laguipo3@mail.dcu.ie>
2020-12-16 22:43:05 +0000

    Added StaffTable and corresponding data

---
+++ src/components/StaffTable.jsx
@@ -0,0 +1,56 @@
+
+import { Component, React } from 'react';
+import data from "./staffData"
+import DataTable from 'react-data-table-component';
+
+const loggedStudent = "test1"
+
+const columns = [
+    {
+        name: "Company",
+        selector: "company",
+        sortable: false
+    },
+    {
+        name: "Company Contact Number",
+        selector: "company_contact_number",
+        sortable: false
+    },
+    {
+        name: "Student Applicants",
+        selector: "applied_students",
+        sortable: false
+    },
+    {
+        name: "Send CV to Company"
+    },
+    {
+        name: "Student Secured Placements",
+        selector: "successful_placement",
+        sortable: false
+    }
+
+
+]
+
+const ExpandableComponent = ({ data }) => <p >{ data.description }</p>;
+
+
+export default class Table extends Component {
+    render() {
+        return (
+            <DataTable
+            title="Jobs"
+            columns={columns}
+            data={data}
+            keyField="company"
+            expandableRows
+            highlightOnHover
+            expandOnRowClicked
+            defaultSortField="company"
+            expandableRowsComponent={<ExpandableComponent />}
+            theme="dark"
+        />
+        )
+    }
+    };
---
+++ src/components/staffData.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        "company": "Google",
+        "company_contact_number": "0853422932",
+        "applied_students": ["John"],
+        "successful_placement": ["Jamie, ", "Tinh, ", "Matthew, ","Cody" ],
+        "description": " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget commodo libero. Mauris congue consectetur vulputate. Maecenas magna dui, ultrices id fringilla ut, pulvinar eget ex. Aenean eu urna neque. Maecenas quis diam eu elit maximus consectetur eget et sem. Vivamus in purus dignissim, pharetra magna ut, vehicula urna. Pellentesque non bibendum nisl. Donec volutpat ornare eros id commodo. Cras ut venenatis massa, imperdiet pellentesque magna. "
+
+
+    },
+    {
+        "company": "Amazon",
+        "company_contact_number": "0862498231",
+        "applied_students": ["Michael, ", "Darragh"],
+        "successful_placement": [],
+        "description": " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget commodo libero. Mauris congue consectetur vulputate. Maecenas magna dui, ultrices id fringilla ut, pulvinar eget ex. Aenean eu urna neque. Maecenas quis diam eu elit maximus consectetur eget et sem. Vivamus in purus dignissim, pharetra magna ut, vehicula urna. Pellentesque non bibendum nisl. Donec volutpat ornare eros id commodo. Cras ut venenatis massa, imperdiet pellentesque magna. "
+    },
+    {
+        "company": "Demonware",
+        "company_contact_number": "0872392123",
+        "applied_students": ["Nicole"],
+        "successful_placement": [],
+        "description": " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget commodo libero. Mauris congue consectetur vulputate. Maecenas magna dui, ultrices id fringilla ut, pulvinar eget ex. Aenean eu urna neque. Maecenas quis diam eu elit maximus consectetur eget et sem. Vivamus in purus dignissim, pharetra magna ut, vehicula urna. Pellentesque non bibendum nisl. Donec volutpat ornare eros id commodo. Cras ut venenatis massa, imperdiet pellentesque magna. "
+    }]
+
+export default data
